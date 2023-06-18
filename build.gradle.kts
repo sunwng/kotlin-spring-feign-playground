a9242992b5c18f3374b69c2a0b9d3cda80d61e7d
sunwng <sunwgna@gmail.com>
2023-06-18 22:28:33 +0900

    feat: 멀티 모듈 Gradle 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,60 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// todo: 이후에 버전 정보 gradle.properties 로 추출해보기
 plugins {
-    kotlin("jvm") version "1.7.10"
+    id("org.springframework.boot") version "2.5.14" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0" apply false
+    kotlin("jvm") version "1.7.10" apply false
+    kotlin("kapt") version "1.7.10" apply false
+    kotlin("plugin.spring") version "1.7.10" apply false
+    kotlin("plugin.jpa") version "1.7.10" apply false
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "org.example"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    dependencies {
+        val implementation by configurations
+        val testImplementation by configurations
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+//        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation(kotlin("test"))
+    }
+
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 
 rootProject.name = "kotlin-spring-feign-playground"
 
+include("sample-server-first", "sample-server-second")
